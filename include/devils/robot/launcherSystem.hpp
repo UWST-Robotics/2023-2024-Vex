a103947198fafe9f03bec1bbd33eb6b9c83c14c9
Digi <digiworm0@gmail.com>
2024-01-25 18:17:43 -0600

    Converted Catapult to Launcher

---
+++ include/devils/robot/launcherSystem.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include "../hardware/smartMotor.hpp"
+
+namespace devils
+{
+    /**
+     * Controls the launcher system to launch triballs.
+     */
+    class LauncherSystem
+    {
+    public:
+        /**
+         * Creates a new launcher system.
+         * @param leftMotorPort The port of the left launcher motor
+         * @param rightMotorPort The port of the right launcher motor
+         */
+        LauncherSystem(const int8_t leftMotorPort, const int8_t rightMotorPort)
+            : leftMotor("Left Launcher Motor", leftMotorPort),
+              rightMotor("Right Launcher Motor", rightMotorPort)
+        {
+        }
+
+        /**
+         * Runs the launcher motors
+         */
+        void fire()
+        {
+            leftMotor.moveVoltage(MOTOR_SPEED);
+            rightMotor.moveVoltage(-MOTOR_SPEED);
+            isFiring = true;
+        }
+
+        /**
+         * Stops the launcher motors
+         */
+        void stop()
+        {
+            leftMotor.moveVoltage(0);
+            rightMotor.moveVoltage(0);
+            isFiring = false;
+        }
+
+        /**
+         * Returns true if the launcher is firing.
+         */
+        const bool getFiring()
+        {
+            return isFiring;
+        }
+
+    private:
+        static constexpr double MOTOR_SPEED = 1.0;
+
+        bool isFiring = false;
+        SmartMotor leftMotor;
+        SmartMotor rightMotor;
+    };
+}
