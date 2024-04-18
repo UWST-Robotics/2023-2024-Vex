26ca4bf5040cacdf7d9e2f5de6b450c1dbe3c304
Digi <aus343@gmail.com>
2024-04-18 15:53:13 -0500

    Updated PROS

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ void initialize()
 {
 	Logger::init();
 	Logger::info("==== Initialize ====");
-	robot = std::make_shared<BlazeTest>(); // <-- Change this to the target robot
+	robot = std::make_shared<Blaze>(); // <-- Change this to the target robot
 }
 
 /**
